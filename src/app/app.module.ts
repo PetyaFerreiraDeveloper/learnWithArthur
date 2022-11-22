b34d22c970509f0f67a33e89c7d6004fa658edc4
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2022-11-22 15:41:05 +0100

    create modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 
